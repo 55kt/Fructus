2c3e39e34b64d12c1868e4f9987fd49906e36cfc
Vlad <160039489+55kt@users.noreply.github.com>
2024-12-08 19:57:58 +0200

    First Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Vlad on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
